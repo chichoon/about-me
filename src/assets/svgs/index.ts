ab4fa9f35b965e7a687b3fb5c1e8cc83cc78712e
chichoon <chichoon.choi@gmail.com>
2022-07-12 17:49:45 +0900

    Design: 아이콘 추가

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as BlogIcon } from './blogIcon.svg';
+export { ReactComponent as EmailIcon } from './emailIcon.svg';
+export { ReactComponent as GithubIcon } from './githubIcon.svg';
