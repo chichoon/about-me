d0402d2d3c91a35ec4e04e9b1b177f1c12568c79
chichoon <chichoon.choi@gmail.com>
2023-04-03 00:00:11 +0900

    chore: :truck: Head 를 Layout 으로 이동

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,21 @@
+import Head from 'next/head';
+
 interface Props {
   children: JSX.Element;
 }
 
 const Layout = ({ children }: Props) => {
-  return <main>{children}</main>;
+  return (
+    <>
+      <Head>
+        <title>About Chichoon</title>
+        <meta name='description' content='치춘에 관한 소개글' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>{children}</main>
+    </>
+  );
 };
 
 export default Layout;
