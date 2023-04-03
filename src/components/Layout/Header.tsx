420dee03ca23a9ea7f034525e9ad0a2c8af7a962
chichoon <chichoon.choi@gmail.com>
2023-04-03 22:35:52 +0900

    style: :lipstick: 헤더 스타일 조정

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,23 +1,25 @@
-import { EmailIcon, GithubIcon } from '@/assets/svgs';
 import Image from 'next/image';
+
+import { EmailIcon, GithubIcon } from '@/assets/svgs';
+import myInfo from '@/assets/myInfo.json';
 
 import styles from './Header.module.scss';
 
 export const Header = ({}) => {
   return (
     <header className={styles.headerWrapper}>
-      <Image src='/profile.jpg' alt='chichoon profile' width={125} height={125} className={styles.headerImage} />
+      <Image src='/profile.jpg' alt='chichoon profile' width={150} height={150} className={styles.headerImage} />
       <div className={styles.headerProfileSection}>
         <div className={styles.headerProfileNamespace}>
-          <h1>최지윤</h1>
-          <a href='mailto:chichoon.choi@gmail.com' target='_blank'>
+          <h1>{myInfo.name}</h1>
+          <a href={`mailto:${myInfo.email}`} target='_blank'>
             <EmailIcon />
           </a>
-          <a href='https://github.com/chichoon' target='_blank'>
+          <a href={myInfo.github} target='_blank'>
             <GithubIcon />
           </a>
         </div>
-        <h2>한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개</h2>
+        <h2 className={styles.headerProfileComment}>{myInfo.bio.title}</h2>
       </div>
     </header>
   );
