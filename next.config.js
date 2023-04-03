480ceac6a23cf0aff69a2bb947fe2937810ae187
chichoon <chichoon.choi@gmail.com>
2023-04-03 21:48:38 +0900

    feat: :heavy_plus_sign: svgr 설치 및 svg 컴포넌트 형태로 불러올 수 있도록 수정

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "@/styles/constants.scss"; @import "@/styles/responsive.scss";`,
   },
+  webpack: (config) => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,5 @@
+import BlogIcon from './blogIcon.svg';
+import EmailIcon from './emailIcon.svg';
+import GithubIcon from './githubIcon.svg';
+
+export { BlogIcon, EmailIcon, GithubIcon };
