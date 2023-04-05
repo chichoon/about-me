2fc8be7fa33c1b68ee853ef6fb9029765d3476a0
chichoon <chichoon.choi@gmail.com>
2023-04-05 21:50:24 +0900

    chore: :truck: index.tsx 추가 및 왼쪽 화살표 아이콘 이동

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,5 +1,6 @@
 import BlogIcon from './blogIcon.svg';
 import EmailIcon from './emailIcon.svg';
 import GithubIcon from './githubIcon.svg';
+import LeftArrowIcon from './leftArrowIcon.svg';
 
-export { BlogIcon, EmailIcon, GithubIcon };
+export { BlogIcon, EmailIcon, GithubIcon, LeftArrowIcon };
---
+++ src/pages/secret/assets/index.ts
@@ -0,0 +1,25 @@
+import CheckIcon from './checkIcon.svg';
+import FastForwardIcon from './fastForwardIcon.svg';
+import HangerIcon from './hangerIcon.svg';
+import HomeIcon from './homeIcon.svg';
+import HourglassIcon from './hourglassIcon.svg';
+import PlusIcon from './plusIcon.svg';
+import RookIcon from './rookIcon.svg';
+import ShieldIcon from './shieldIcon.svg';
+import SquareIcon from './squareIcon.svg';
+import StarIcon from './starIcon.svg';
+import SwordIcon from './swordIcon.svg';
+
+export {
+  CheckIcon,
+  FastForwardIcon,
+  HangerIcon,
+  HomeIcon,
+  HourglassIcon,
+  PlusIcon,
+  RookIcon,
+  ShieldIcon,
+  SwordIcon,
+  StarIcon,
+  SquareIcon,
+};
