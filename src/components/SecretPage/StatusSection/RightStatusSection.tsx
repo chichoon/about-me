b9e15593c6f7d6e819a25c76371a985437c4b8b8
chichoon <chichoon.choi@gmail.com>
2023-04-09 00:29:14 +0900

    style: :lipstick: 스킬 3칸 완성

--- src/components/SecretPage/StatusSection/RightStatusSection.tsx
+++ src/components/SecretPage/StatusSection/RightStatusSection.tsx
@@ -1,21 +1,22 @@
 import { css } from '@emotion/react';
 
 import { SECRET_LEVELS } from '@/components/SecretPage/constants';
-import { SkillsAndTalentStats } from './SkillsAndTalentStats';
 import { ExpStats } from './ExpStats.tsx';
 import { EliteStats } from './EliteStats';
+import { SkillStats } from './SkillStats';
 
 export const RightStatusSection = () => {
   return (
     <section css={rightStatSectionWrapper}>
       <ExpStats />
       <EliteStats />
-      <SkillsAndTalentStats />
+      <SkillStats />
     </section>
   );
 };
 
 export const rightStatSectionWrapper = css({
   maxWidth: '50vw',
+  padding: 20,
   zIndex: SECRET_LEVELS.LEVEL_UI,
 });
