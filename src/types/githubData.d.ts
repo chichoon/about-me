9f09561ef77e13695fbfcb16afb0272a25dfac09
chichoon <chichoon.choi@gmail.com>
2023-04-10 23:55:11 +0900

    fix: :rotating_light: githubData 내부 타입이 제대로 포맷되어있지 않은 문제 수정

--- src/types/githubData.d.ts
+++ src/types/githubData.d.ts
@@ -1,14 +1,14 @@
-interface  GithubDataType = {
-	login: string;
-	name: string;
-	blog: string;
+interface GithubDataType {
+  login: string;
+  name: string;
+  blog: string;
 
-	public_repos: number;
-	followers: number;
-	following: number;
-	totalCommits: number;
-	commitsThisYear: number;
-	totalStarsEarned: number;
-	totalPullRequests: number;
-	totalIssues: number;
+  public_repos: number;
+  followers: number;
+  following: number;
+  totalCommits: number;
+  commitsThisYear: number;
+  totalStarsEarned: number;
+  totalPullRequests: number;
+  totalIssues: number;
 }
