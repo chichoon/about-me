23c91e709791c71795a4f9e7d4ee6ed8fa52a788
chichoon <chichoon.choi@gmail.com>
2023-04-13 19:58:39 +0900

    chore: :truck: styles.ts 없애고 전부 컴포넌트와 디자인 합침

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,5 +1,8 @@
+import { css } from '@emotion/react';
+
 import { GithubIcon } from '@/assets/svgs';
-import { footerWrapperStyle } from './Footer.styles';
+import { COLORS } from '@/styles/colors';
+import { SIZES } from '@/styles/sizes';
 
 interface Props {
   githubUsername: string;
@@ -16,3 +19,31 @@ export const Footer = ({ githubUsername, bioGithubLink }: Props) => {
     </footer>
   );
 };
+
+const footerWrapperStyle = css({
+  width: '100vw',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: 50,
+
+  span: {
+    color: COLORS.GRAY9,
+    marginRight: 10,
+    fontSize: SIZES.$FONT_M,
+    userSelect: 'none',
+    cursor: 'default',
+  },
+
+  a: {
+    padding: 0,
+    margin: 0,
+    width: 20,
+    height: 20,
+  },
+
+  svg: {
+    fill: COLORS.GRAY9,
+  },
+});
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,12 @@
-import { Header } from './Header';
+import { css } from '@emotion/react';
 
 import { ProfileType } from '@/types/profileData';
+import { Header } from './Header';
 import { Footer } from './Footer';
 import { HeadMetaData } from './HeadMetaData';
 
-import { mainWrapperStyle } from './Layout.styles';
+import { COLORS } from '@/styles/colors';
+import { getResponsiveAfter } from '@/styles/getResponsiveBreakpoint';
 
 interface Props {
   children: JSX.Element;
@@ -23,3 +25,21 @@ export const Layout = ({ children, profileData }: Props) => {
     </>
   );
 };
+
+const mainWrapperStyle = css({
+  padding: '0 25px',
+
+  [getResponsiveAfter('SD')]: {
+    padding: '0 100px',
+  },
+
+  '> div': {
+    backgroundColor: COLORS.WHITE,
+    padding: 25,
+    borderRadius: 5,
+
+    [getResponsiveAfter('SD')]: {
+      padding: 50,
+    },
+  },
+});
