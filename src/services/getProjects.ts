d5c6bdbe0d963de33dd3c76af92029d8a5cb2607
chichoon <chichoon.choi@gmail.com>
2023-04-14 00:39:39 +0900

    chore: :poop: 서버 URL 체크

--- src/services/getProjects.ts
+++ src/services/getProjects.ts
@@ -1,6 +1,7 @@
 const serverUrl = process.env.NODE_ENV === 'production' ? process.env.VERCEL_URL : process.env.DEV_URL;
 
 export async function getProjects() {
+  console.log(serverUrl);
   const data = await fetch(`${serverUrl}/projectsInfo.json`).then((response) => response.json());
 
   return data;
