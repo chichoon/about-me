67ca75d2c4117cf0fe081cb72113e502ee920368
chichoon <chichoon.choi@gmail.com>
2023-04-18 18:48:20 +0900

    chore: :truck: homeIcon 을 assets/svgs로 이동

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,5 +2,6 @@ import BlogIcon from './blogIcon.svg';
 import EmailIcon from './emailIcon.svg';
 import GithubIcon from './githubIcon.svg';
 import LeftArrowIcon from './leftArrowIcon.svg';
+import HomeIcon from './homeIcon.svg';
 
-export { BlogIcon, EmailIcon, GithubIcon, LeftArrowIcon };
+export { BlogIcon, EmailIcon, GithubIcon, LeftArrowIcon, HomeIcon };
