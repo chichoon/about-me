9644d18af12c1b2cd33e0ecc1cd96e01e5132855
chichoon <chichoon.choi@gmail.com>
2023-04-20 16:54:42 +0900

    style: :lipstick: hide overflow content

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -158,6 +158,8 @@ const headerProfileCommentStyle = css({
   display: '-webkit-box',
   width: '100%',
   fontSize: SIZES.$FONT_M,
+  lineHeight: `${SIZES.$FONT_M}px`,
+  height: SIZES.$FONT_M * 2,
   overflow: 'hidden',
   lineClamp: 2,
   WebkitBoxOrient: 'vertical',
@@ -165,10 +167,13 @@ const headerProfileCommentStyle = css({
 
   [getResponsiveAfter('ML')]: {
     fontSize: SIZES.$FONT_L,
+    lineHeight: `${SIZES.$FONT_L}px`,
+    height: SIZES.$FONT_L * 2,
   },
 
   [getResponsiveAfter('SD')]: {
     lineClamp: 1,
     WebkitLineClamp: 1,
+    height: SIZES.$FONT_L,
   },
 });
