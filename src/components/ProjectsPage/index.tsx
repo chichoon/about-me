69da18a186d31be98af0a9b9d6174eb4841e7d6a
chichoon <chichoon.choi@gmail.com>
2023-04-20 17:53:43 +0900

    style: :lipstick: projects page branch add

--- src/components/ProjectsPage/index.tsx
+++ src/components/ProjectsPage/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
+import { css } from '@emotion/react';
 
 import { ProjectKeyAndTitleType } from '@/types/profileData';
+
+import { COLORS } from '@/styles/colors';
+import { LEVELS } from '@/styles/levels';
 
 interface Props {
   projectKeysAndTitles: ProjectKeyAndTitleType[];
@@ -8,9 +12,9 @@ interface Props {
 
 export const ProjectsPage = ({ projectKeysAndTitles }: Props) => {
   return (
-    <menu>
-      {projectKeysAndTitles.map(({ key, title }) => (
-        <li key={`experience-${key}`}>
+    <menu css={projectPageWrapperStyle(100)}>
+      {projectKeysAndTitles.map(({ key, title }, index) => (
+        <li key={`experience-${key}`} css={projectElementStyle(index * 50)}>
           <Link href={`/projects/${key}`}>
             <h3>{title}</h3>
           </Link>
@@ -19,3 +23,33 @@ export const ProjectsPage = ({ projectKeysAndTitles }: Props) => {
     </menu>
   );
 };
+
+const projectPageWrapperStyle = (height: number) =>
+  css({
+    position: 'relative',
+    height,
+
+    ':after': {
+      content: '""',
+      display: 'block',
+      position: 'absolute',
+      zIndex: LEVELS.MAINSTREAM_BRANCH,
+      top: 10,
+      left: 25,
+      width: 10,
+      height,
+      backgroundColor: COLORS.GRAY9,
+    },
+  });
+
+const projectElementStyle = (topPosition: number) =>
+  css({
+    position: 'absolute',
+    zIndex: LEVELS.SUB_BRANCH,
+    top: topPosition,
+
+    a: {
+      width: '100%',
+      height: '100%',
+    },
+  });
