d70c49f5331fc62dee0e1ed22700c2d863971c8e
chichoon <chichoon.choi@gmail.com>
2023-04-22 12:47:54 +0900

    feat: :sparkles: 헤더 정보 조금 추가

--- src/components/Layout/HeadMetaData.tsx
+++ src/components/Layout/HeadMetaData.tsx
@@ -4,7 +4,6 @@ export const HeadMetaData = () => {
   return (
     <Head>
       <title>About Ji Yoon Choi</title>
-      <meta name='description' content='About Ji Yoon Choi' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <link rel='apple-touch-icon' sizes='57x57' href='/icon/apple-icon-57x57.png' />
       <link rel='apple-touch-icon' sizes='60x60' href='/icon/apple-icon-60x60.png' />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Layout, MainPage } from '@/components';
 import { getProfile } from '@/services';
 import { ProfileType } from '@/types/profileData';
+import Head from 'next/head';
 
 export async function getStaticProps() {
   const data = await getProfile();
@@ -17,7 +18,18 @@ const Home = ({ data }: Props) => {
   const { profileImageRef, summaries, stacks } = data;
   return (
     <Layout profileData={data}>
-      <MainPage profileImageRef={profileImageRef} summaries={summaries} stacks={stacks} />
+      <>
+        <Head>
+          <meta name='title' content='About Ji Yoon Choi' />
+          <meta name='description' content='내가 프론트엔드에 발을 들이게 된 계기' />
+          <meta name='keywords' content='자기소개' />
+          <meta name='robots' content='index, follow' />
+          <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
+          <meta name='language' content='Korean' />
+          <meta name='author' content='chichoon' />
+        </Head>
+        <MainPage profileImageRef={profileImageRef} summaries={summaries} stacks={stacks} />
+      </>
     </Layout>
   );
 };
