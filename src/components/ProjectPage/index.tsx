f7fabe8a9d5223e3b1649a1f030085c06d73a382
chichoon <chichoon.choi@gmail.com>
2023-04-22 17:48:41 +0900

    feat: :sparkles: 프로젝트별 기술스택 뱃지 출력하는 컴포넌트 추가

--- src/components/ProjectPage/index.tsx
+++ src/components/ProjectPage/index.tsx
@@ -1,5 +1,6 @@
 import { ProjectType } from '@/types/profileData';
 import { ProjectInfoHeader } from './ProjectInfoHeader';
+import { BadgeList } from '../BadgeList';
 
 interface Props {
   project: ProjectType;
@@ -9,6 +10,7 @@ export const ProjectPage = ({ project }: Props) => {
   return (
     <>
       <ProjectInfoHeader project={project} />
+      <BadgeList stacks={project.stacks} />
       <div>hihi</div>
     </>
   );
