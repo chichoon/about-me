93cad8b6122e3ccdc9a3e30a9d04abc9dcc93ca2
chichoon <chichoon.choi@gmail.com>
2023-05-04 14:30:27 +0900

    style: :lipstick: 브랜치 길이 조절

--- src/components/BranchListWrapper.tsx
+++ src/components/BranchListWrapper.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { css } from '@emotion/react';
 
 import { COLORS } from '@/styles/colors';
@@ -10,60 +11,64 @@ interface Props {
 }
 
 export const BranchListWrapper = ({ children }: Props) => {
-  return <menu css={projectPageMenuStyle}>{children}</menu>;
+  const router = useRouter();
+  const BRANCH_MAINSTREAM_HEIGHT =
+    router.pathname === '/projects' ? SIZES.BRANCH_MAINSTREAM_PROJECT_HEIGHT : SIZES.BRANCH_MAINSTREAM_FOOTPRINT_HEIGHT;
+  return <menu css={projectPageMenuStyle(BRANCH_MAINSTREAM_HEIGHT)}>{children}</menu>;
 };
 
-const projectPageMenuStyle = css({
-  position: 'relative',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  paddingLeft: 10,
-  width: 'calc(100% - 10px)',
-  height: SIZES.BRANCH_MAINSTREAM_HEIGHT + 150,
-  overflowX: 'scroll',
+const projectPageMenuStyle = (height: number) =>
+  css({
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    paddingLeft: 10,
+    width: 'calc(100% - 10px)',
+    height: height + 150,
+    overflowX: 'scroll',
 
-  ':before': {
-    content: '""',
-    display: 'block',
-    zIndex: LEVELS.MAINSTREAM_BRANCH,
-    width: 10,
-    height: SIZES.BRANCH_MAINSTREAM_HEIGHT + 100,
-    background: `linear-gradient(to top, ${COLORS.GRAY9} 90%, ${COLORS.WHITE} 100% )`,
-  },
-
-  ':after': {
-    content: '""',
-    backgroundColor: COLORS.WHITE,
-    zIndex: LEVELS.SUB_BRANCH,
-    marginLeft: -10,
-    marginTop: -1,
-    width: 20,
-    height: 20,
-    borderRadius: 25,
-    border: `5px solid ${COLORS.GRAY9}`,
-  },
-
-  [getResponsiveAfter('ML')]: {
-    paddingLeft: 25,
-    width: 370,
+    ':before': {
+      content: '""',
+      display: 'block',
+      zIndex: LEVELS.MAINSTREAM_BRANCH,
+      width: 10,
+      height: height + 100,
+      background: `linear-gradient(to top, ${COLORS.GRAY9} 90%, ${COLORS.WHITE} 100% )`,
+    },
 
     ':after': {
-      marginLeft: -20,
-      width: 40,
-      height: 40,
+      content: '""',
+      backgroundColor: COLORS.WHITE,
+      zIndex: LEVELS.SUB_BRANCH,
+      marginLeft: -10,
+      marginTop: -1,
+      width: 20,
+      height: 20,
+      borderRadius: 25,
+      border: `5px solid ${COLORS.GRAY9}`,
     },
-  },
 
-  [getResponsiveAfter('LD')]: {
-    paddingLeft: 25,
-    width: 370,
-    marginRight: 50,
+    [getResponsiveAfter('ML')]: {
+      paddingLeft: 25,
+      width: 370,
 
-    ':after': {
-      marginLeft: -20,
-      width: 40,
-      height: 40,
+      ':after': {
+        marginLeft: -20,
+        width: 40,
+        height: 40,
+      },
     },
-  },
-});
+
+    [getResponsiveAfter('LD')]: {
+      paddingLeft: 25,
+      width: 370,
+      marginRight: 50,
+
+      ':after': {
+        marginLeft: -20,
+        width: 40,
+        height: 40,
+      },
+    },
+  });
