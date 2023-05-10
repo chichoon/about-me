d4ec34d08d430ba38327804c1117b00389ba388a
chichoon <chichoon.choi@gmail.com>
2023-05-11 02:38:48 +0900

    feat: :sparkles: introduction page 작성완료

---
+++ src/app/introduction/page.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import { Layout, Paragraph } from '@/components';
+import { getProfile } from '@/services';
+
+import styles from './introductionPage.module.scss';
+
+const IntroductionPage = async () => {
+  const data = await getProfile();
+  return (
+    <Layout profileData={data}>
+      <>
+        <Head>
+          <meta name='title' content='Introduction of chichoon' />
+          <meta name='description' content='치춘의 간단 소개' />
+          <meta name='keywords' content='Introduction Page' />
+        </Head>
+        <Image src={data.profileImageRef} alt='Profile' width={200} height={200} className={styles.image} />
+        <Paragraph paragraph={data.summaries} lineGap={25} />
+      </>
+    </Layout>
+  );
+};
+
+export default IntroductionPage;
