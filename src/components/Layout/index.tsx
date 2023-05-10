ca040127ee84428cb9bd9c01af9fdba4e916078e
chichoon <chichoon.choi@gmail.com>
2023-05-11 02:15:28 +0900

    refactor: :truck: 레이아웃 인덱스 emotion에서 scss로 수정

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,6 +3,8 @@ import { Header } from './Header';
 import { Footer } from './Footer';
 import { HeadMetaData } from './HeadMetaData';
 
+import styles from './layout.module.scss';
+
 interface Props {
   children: JSX.Element;
   profileData: ProfileType;
@@ -15,39 +17,10 @@ export const Layout = ({ children, profileData }: Props) => {
     <>
       <HeadMetaData />
       <Header {...profileData} />
-      {/* <main css={mainWrapperStyle}>
-        <div css={mainInnerStyle}>{children}</div>
-      </main> */}
-      <main>
-        <div>{children}</div>
+      <main className={styles.mainWrapper}>
+        <div className={styles.mainInner}>{children}</div>
       </main>
       <Footer githubUsername={githubUsername} bioGithubLink={bioGithubLink} />
     </>
   );
 };
-
-// const mainWrapperStyle = css({
-//   padding: '0 25px',
-
-//   [getResponsiveAfter('SD')]: {
-//     padding: '0 100px',
-//   },
-// });
-
-// const mainInnerStyle = css({
-//   display: 'flex',
-//   flexDirection: 'column',
-//   alignItems: 'center',
-//   backgroundColor: COLORS.WHITE,
-//   padding: 25,
-//   borderRadius: 5,
-//   overflowX: 'hidden',
-
-//   [getResponsiveAfter('ML')]: {
-//     display: 'block',
-//   },
-
-//   [getResponsiveAfter('SD')]: {
-//     padding: 50,
-//   },
-// });
