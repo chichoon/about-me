79e69b5d84a3ee8d64f981b0115ad57171ec35e6
chichoon <chichoon.choi@gmail.com>
2023-05-11 01:54:30 +0900

    refactor: :truck: header emotion에서 scss로 수정

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
-import { css } from '@emotion/react';
+
+import { NavBar } from './NavBar';
 
 import { BlogIcon, EmailIcon, GithubIcon } from '@/assets/svgs';
-import { COLORS } from '@/styles/colors';
-import { getResponsiveAfter } from '@/styles/getResponsiveBreakpoint';
-import { SIZES } from '@/styles/sizes';
-import { NavBar } from './NavBar';
+
+import styles from './header.module.scss';
 
 interface Props {
   name: string;
@@ -18,10 +17,17 @@ interface Props {
 export const Header = ({ name, email, oneLineBio, githubUsername, blogLink }: Props) => {
   return (
     <>
-      <header css={headerWrapperStyle}>
-        <Image priority src='/profile.jpg' alt='chichoon profile' width={150} height={150} css={headerImageStyle} />
-        <div css={headerProfileSectionStyle}>
-          <div css={headerProfileNamespaceStyle}>
+      <header className={styles.headerWrapper}>
+        <Image
+          priority
+          src='/profile.jpg'
+          alt='chichoon profile'
+          width={150}
+          height={150}
+          className={styles.headerImage}
+        />
+        <div className={styles.headerProfileSection}>
+          <div className={styles.headerProfileNamespace}>
             <h1>{name}</h1>
             <a href={`mailto:${email}`} target='_blank'>
               <EmailIcon />
@@ -33,149 +39,10 @@ export const Header = ({ name, email, oneLineBio, githubUsername, blogLink }: Pr
               <BlogIcon />
             </a>
           </div>
-          <h2 css={headerProfileCommentStyle}>{oneLineBio}</h2>
+          <h2 className={styles.headerProfileComment}>{oneLineBio}</h2>
         </div>
       </header>
       <NavBar />
     </>
   );
 };
-
-const headerWrapperStyle = css({
-  backgroundColor: COLORS.WHITE,
-  display: 'flex',
-  flexDirection: 'row',
-  width: '100vw',
-  height: 100,
-  padding: '0 25px',
-  marginBottom: 25,
-
-  [getResponsiveAfter('M')]: {
-    height: 150,
-  },
-
-  [getResponsiveAfter('SD')]: {
-    padding: '0 100px',
-  },
-});
-
-const headerImageStyle = css({
-  borderRadius: 5,
-  display: 'none',
-
-  [getResponsiveAfter('M')]: {
-    display: 'block',
-    marginTop: 50,
-    width: 100,
-    height: 100,
-  },
-
-  [getResponsiveAfter('ML')]: {
-    display: 'block',
-    marginTop: 25,
-    width: 125,
-    height: 125,
-  },
-
-  [getResponsiveAfter('SD')]: {
-    marginTop: 50,
-    width: 150,
-    height: 150,
-  },
-});
-
-const headerProfileSectionStyle = css({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'flex-end',
-  width: 300,
-  height: 100,
-  marginTop: 0,
-  padding: 10,
-  overflowX: 'scroll',
-  overflowY: 'hidden',
-
-  [getResponsiveAfter('M')]: {
-    marginTop: 25,
-    marginLeft: 25,
-    padding: 0,
-  },
-
-  [getResponsiveAfter('SD')]: {
-    justifyContent: 'flex-start',
-    width: SIZES.MAIN_INNER_MAX_WIDTH,
-    marginTop: 50,
-  },
-});
-
-const headerProfileNamespaceStyle = css({
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  marginBottom: 10,
-
-  h1: {
-    fontSize: SIZES.FONT_L,
-    color: COLORS.GRAY3,
-  },
-
-  a: {
-    padding: 2.5,
-    width: 25,
-    height: 25,
-    marginLeft: 15,
-  },
-
-  svg: {
-    width: 20,
-    height: 20,
-  },
-
-  [getResponsiveAfter('ML')]: {
-    h1: {
-      fontSize: SIZES.FONT_XL,
-    },
-
-    a: {
-      padding: 2.5,
-      width: 30,
-      height: 30,
-      marginLeft: 15,
-    },
-
-    svg: {
-      width: 25,
-      height: 25,
-    },
-  },
-
-  [getResponsiveAfter('SD')]: {
-    width: 700,
-    marginTop: 25,
-  },
-});
-
-const headerProfileCommentStyle = css({
-  display: '-webkit-box',
-  width: '100%',
-  fontSize: SIZES.FONT_M,
-  color: COLORS.GRAY3,
-  lineHeight: `${SIZES.FONT_M}px`,
-  height: SIZES.FONT_M * 2,
-  overflow: 'hidden',
-  lineClamp: 2,
-  WebkitBoxOrient: 'vertical',
-  WebkitLineClamp: 2,
-
-  [getResponsiveAfter('ML')]: {
-    fontSize: SIZES.FONT_L,
-    lineHeight: `${SIZES.FONT_L}px`,
-    height: SIZES.FONT_L * 2,
-  },
-
-  [getResponsiveAfter('SD')]: {
-    lineClamp: 1,
-    WebkitLineClamp: 1,
-    height: SIZES.FONT_L,
-  },
-});
--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -9,20 +9,17 @@ export const NavBar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className={styles.navTabsStyle}>
-      <Link href='/' className={`${styles.tabStyle} ${pathname === '/main' ? styles.selected : ''}`}>
+    <nav className={styles.navTabs}>
+      <Link href='/' className={`${styles.tab} ${pathname === '/main' ? styles.selected : ''}`}>
         <span>홈</span>
       </Link>
-      <Link
-        href='/introduction'
-        className={`${styles.tabStyle} ${pathname === '/introduction' ? styles.selected : ''}`}
-      >
+      <Link href='/introduction' className={`${styles.tab} ${pathname === '/introduction' ? styles.selected : ''}`}>
         <span>소개</span>
       </Link>
-      <Link href='/projects' className={`${styles.tabStyle} ${pathname === '/projects' ? styles.selected : ''}`}>
+      <Link href='/projects' className={`${styles.tab} ${pathname === '/projects' ? styles.selected : ''}`}>
         <span>프로젝트</span>
       </Link>
-      <Link href='/footprints' className={`${styles.tabStyle} ${pathname === '/footprints' ? styles.selected : ''}`}>
+      <Link href='/footprints' className={`${styles.tab} ${pathname === '/footprints' ? styles.selected : ''}`}>
         <span>발자취</span>
       </Link>
     </nav>
