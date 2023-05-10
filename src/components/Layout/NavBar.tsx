e7f89c1adea83b45872f4726f0e90cd6510c87ed
chichoon <chichoon.choi@gmail.com>
2023-05-11 01:35:07 +0900

    refactor: :truck: navbar emotion 쓰여있던 부분 scss로 수정

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,80 +1,30 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { css } from '@emotion/react';
+'use client';
 
-import { getResponsiveAfter } from '@/styles/getResponsiveBreakpoint';
-import { COLORS } from '@/styles/colors';
-import { SIZES } from '@/styles/sizes';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import styles from './navBar.module.scss';
 
 export const NavBar = () => {
-  const router = useRouter();
+  const pathname = usePathname();
 
   return (
-    <nav css={navTabsStyle}>
-      <Link href='/' css={tabStyle(router.pathname === '/')}>
+    <nav className={styles.navTabsStyle}>
+      <Link href='/' className={`${styles.tabStyle} ${pathname === '/main' ? styles.selected : ''}`}>
         <span>홈</span>
       </Link>
-      <Link href='/introduction' css={tabStyle(router.pathname === '/introduction')}>
+      <Link
+        href='/introduction'
+        className={`${styles.tabStyle} ${pathname === '/introduction' ? styles.selected : ''}`}
+      >
         <span>소개</span>
       </Link>
-      <Link href='/projects' css={tabStyle(router.pathname.startsWith('/projects'))}>
+      <Link href='/projects' className={`${styles.tabStyle} ${pathname === '/projects' ? styles.selected : ''}`}>
         <span>프로젝트</span>
       </Link>
-      <Link href='/footprints' css={tabStyle(router.pathname.startsWith('/footprints'))}>
+      <Link href='/footprints' className={`${styles.tabStyle} ${pathname === '/footprints' ? styles.selected : ''}`}>
         <span>발자취</span>
       </Link>
     </nav>
   );
 };
-
-const navTabsStyle = css({
-  width: '100vw',
-  height: 40,
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-  padding: '0 50px',
-
-  [getResponsiveAfter('M')]: {
-    height: 50,
-  },
-
-  [getResponsiveAfter('SD')]: {
-    padding: '0 75px 0 275px',
-  },
-});
-
-const tabStyle = (isSelected: boolean) =>
-  css({
-    background: isSelected ? COLORS.WHITE : 'none',
-    flex: 1,
-    maxWidth: 100,
-    height: 40,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    transition: 'background 0.2s linear',
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5,
-
-    span: {
-      userSelect: 'none',
-      fontSize: SIZES.FONT_MS,
-      color: isSelected ? COLORS.GRAY3 : COLORS.GRAY9,
-    },
-
-    ':hover': {
-      backgroundColor: isSelected ? COLORS.WHITE : COLORS.GRAYE,
-    },
-
-    [getResponsiveAfter('M')]: {
-      width: 100,
-      height: 50,
-
-      span: {
-        fontSize: SIZES.FONT_M,
-      },
-    },
-  });
