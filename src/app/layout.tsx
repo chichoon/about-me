3ef490588980a8bddc044afa91d084194f5a0c96
chichoon <chichoon.choi@gmail.com>
2023-05-10 21:54:25 +0900

    feat: :sparkles: app 라우터로 마이그레이션

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import { Noto_Sans_KR } from 'next/font/google';
+
+import '@/styles/reset.css';
+
+const noto = Noto_Sans_KR({ subsets: ['latin'], weight: '400' });
+
+interface Props {
+  children: JSX.Element;
+}
+
+const RootLayout = ({ children }: Props) => {
+  return (
+    <html lang='ko'>
+      <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#ffffff' />
+        <meta name='robots' content='index, follow' />
+        <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta name='language' content='Korean' />
+        <meta name='author' content='chichoon' />
+      </head>
+      <body className={noto.className}>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const MainPage = () => {
+  return <div>하이</div>;
+};
+
+export default MainPage;
