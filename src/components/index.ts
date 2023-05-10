340999462ad538df47d998950fbb0dd8dab761b5
chichoon <chichoon.choi@gmail.com>
2023-05-11 02:23:02 +0900

    refactor: :truck: SimpleIcon emotion에서 scss로 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,4 @@
 export { Layout } from './Layout';
-export { ErrorPage404, ErrorPage500 } from './ErrorPage';
 
-export { MainPage } from './MainPage';
-export { IntroPage } from './IntroPage';
-export { ProjectsPage } from './ProjectsPage';
-export { ProjectPage } from './ProjectPage';
-export { FootprintsPage } from './FootprintsPage';
-export { FootprintPage } from './FootprintPage';
-
-export { GithubStats } from './GithubStats';
-export { ResumePage } from './ResumePage';
+export { BadgeList } from './BadgeList';
+export { LinkButton } from './LinkButton';
