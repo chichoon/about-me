ccf5ca7be8313b6d9bb9696bfe6780d03ff23413
chichoon <chichoon.choi@gmail.com>
2023-05-11 19:11:30 +0900

    fix: :bug: navbar 하이라이트 조건 수정

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -10,16 +10,19 @@ export const NavBar = () => {
 
   return (
     <nav className={styles.navTabs}>
-      <Link href='/' className={`${styles.tab} ${pathname === '/main' ? styles.selected : ''}`}>
+      <Link href='/' className={`${styles.tab} ${pathname.startsWith('/main') ? styles.selected : ''}`}>
         <span>홈</span>
       </Link>
-      <Link href='/introduction' className={`${styles.tab} ${pathname === '/introduction' ? styles.selected : ''}`}>
+      <Link
+        href='/introduction'
+        className={`${styles.tab} ${pathname.startsWith('/introduction') ? styles.selected : ''}`}
+      >
         <span>소개</span>
       </Link>
-      <Link href='/projects' className={`${styles.tab} ${pathname === '/projects' ? styles.selected : ''}`}>
+      <Link href='/projects' className={`${styles.tab} ${pathname.startsWith('/projects') ? styles.selected : ''}`}>
         <span>프로젝트</span>
       </Link>
-      <Link href='/footprints' className={`${styles.tab} ${pathname === '/footprints' ? styles.selected : ''}`}>
+      <Link href='/footprints' className={`${styles.tab} ${pathname.startsWith('/footprints') ? styles.selected : ''}`}>
         <span>발자취</span>
       </Link>
     </nav>
