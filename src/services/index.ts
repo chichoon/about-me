4e311c5883bd61cd312a389cbd0eaa346f9be050
chichoon <chichoon.choi@gmail.com>
2023-07-04 20:31:07 +0900

    feat: :sparkles: footprint resume type으로 가져오는 서비스함수 추가

--- src/services/index.ts
+++ src/services/index.ts
@@ -9,6 +9,7 @@ export { getProjectsAsResumeType } from './getProjectsAsResumeType';
 export { getFootprints } from './getFootprints';
 export { getFootprintIndexes } from './getFootprintIndexes';
 export { getFootprintByIndex } from './getFootprintByIndex';
+export { getFootprintsAsResumeType } from './getFootprintsAsResumeType';
 
 export { getCertificates } from './getCertificates';
 export { getCertificatesAsResumeType } from './getCertificatesAsResumeType';
